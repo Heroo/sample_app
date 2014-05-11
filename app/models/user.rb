f995c94b796b16be26ac40cd3c506b58c6969a51
Heroo <blake@interia.eu>
2014-05-11 17:12:29 +0200

    Finished the password machinery

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,18 +32,44 @@ email_regex = /\A[\w.+\-]+@[a-z\d.\-]+\.[a-z]+\z/i
 
   before_save :encrypt_password
 
+
+  def has_password?(submitted_password)
+encrypted_password == encrypt(submitted_password)
+  end
+
+class << self
+
+  def authenticate(email, submitted_password)
+user= find_by_email(email)
+
+    return nil if user.nil?
+
+    return user if user.has_password?(submitted_password)
+  end
+
+end
+
   private
 
   def encrypt_password
+    self.salt=make_salt if new_record?
+
     self.encrypted_password= encrypt(self.password)
+  end
+
+  def make_salt
+    secure_hash("#{Time.now.utc}--#{password}")
   end
 
   def encrypt(string)
 
-    string  #not the final implementation!
+    secure_hash("#{salt}--#{string}")
 
   end
 
+def secure_hash(string)
+Digest::SHA2.hexdigest(string)
 
+end
   end
 
